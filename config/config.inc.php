27813d4db467b42bd79917a86068ad4d5fb5f838
jessie <junchangchen@gmail.com>
2017-05-15 11:48:15 +0800

    update

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('ADMIN_UID','1');
 
-define ( 'DBHOST', 'localhost');
+define ( 'DBHOST', '127.0.0.1');
 define ( 'DBNAME', 'mweike');
 define ( 'DBUSER', 'weike');
 define ( 'DBPASS', 'weike');
